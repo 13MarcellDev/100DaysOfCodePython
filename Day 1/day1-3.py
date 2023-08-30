59c72a6ecfdad6c4eea2b48b4f5327b539dbec32
13MarcellDev <125213765+13MarcellDev@users.noreply.github.com>
2023-08-30 13:36:24 +0200

    Day 1
    Uploaded the files of day 1.

---
+++ Day 1/day1-3.py
@@ -0,0 +1,7 @@
+characterName = "John"
+characterAge = "35"
+
+print("There once was a man named " + characterName + ", ")
+print("he was " + characterAge + " years old. ")
+print("He really liked the name " + characterName + ", ")
+print("but didn't like being " + characterAge + ".")
